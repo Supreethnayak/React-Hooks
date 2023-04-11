1d1e13e19fea0c4ccf9dd55dc3c7a066b7aa01ab
Supreeth Nayak <59947774+Supreethnayak@users.noreply.github.com>
2023-04-11 15:07:29 +0530

    example with simple state and action added

---
+++ useReducer/1-usereducer-simple-state-and-action/App.js
@@ -0,0 +1,10 @@
+import CounterOne from "./components/CounterOne";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CounterOne />
+    </div>
+  );
+}
---
+++ useReducer/1-usereducer-simple-state-and-action/components/CounterOne.js
@@ -0,0 +1,28 @@
+import { useReducer } from "react";
+const intialState = 0;
+// accepts currentState, action, returns new state
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "increment":
+      return currentState + 1;
+    case "decrement":
+      return currentState - 1;
+    case "reset":
+      return intialState;
+    default:
+      return currentState;
+  }
+};
+export default function CounterOne() {
+  // accepts reducer, intialState, returns [newState,dispatch]
+  const [newState, dispatch] = useReducer(reducer, intialState);
+
+  return (
+    <>
+      <h1>count value is - {newState}</h1>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
+    </>
+  );
+}
